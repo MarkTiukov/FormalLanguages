7f1533fa5a602d0ef7858af5a53d8b1ee71b7864
Mark <Tyukov.mark@gmail.com>
2020-10-05 14:10:28 +0300

    Minimalisation (#3)
    README update

--- Minimization.py
+++ Minimization.py
@@ -24,47 +24,10 @@ def minimize(machine, alphabet, final_states):
             classes = new_classes
     return classes
 
-
-# 4
-# 6
-# a b c
-# 0 1 c
-# 1 1 b
-# 0 2 a
-# 2 1 c
-# 2 3 c
-# 3 3 c
-# 1 3
-
-
 if __name__ == "__main__":
-    from Determination import inputData, determine, printMachine
+    from Determination import inputData, determine, makeTotal
 
     alphabet, machine, final_states = inputData()
-    # dka, new_finals = determine(alphabet, machine, final_states)
-    # total_dka = makeTotal(dka, alphabet)
-    # printMachine(total_dka, new_finals)
-    determined = determine(alphabet, machine, final_states)[0]
-    printMachine(determined, final_states)
-    print(f"<MINIMIZED>:\n{minimize(determined, alphabet, final_states)}")
-
-# 8
-# 16
-# a b
-# 0 1 a
-# 1 2 a
-# 2 2 a
-# 0 5 b
-# 1 3 b
-# 2 7 b
-# 5 5 a
-# 5 5 b
-# 3 1 a
-# 7 2 a
-# 3 4 b
-# 7 6 b
-# 4 3 a
-# 6 7 a
-# 6 4 b
-# 4 5 b
-# 3 4 5 6
+    determined, determined_final_states = determine(alphabet, machine, final_states)
+    print(f"<MINIMIZED>:\n{minimize(makeTotal(determined, alphabet), alphabet, final_states)}")
+    
